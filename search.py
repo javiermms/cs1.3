d86a11b1eb3a9a4c7b0d142e09d9adb1de75c6da
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-06-12 14:20:27 -0700

    finished binary iterative

--- search.py
+++ search.py
@@ -38,10 +38,23 @@ def binary_search(array, item):
 
 
 def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
-    pass
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests
+    
+    low = 0
+    high= len(array) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == item:
+            return mid
+
+        if array[mid] > item:
+            high = mid - 1 # ignores the right
+
+        if array[mid] < item:
+            low = mid + 1 # ignores the left
+
+    return None
+
 
 
 def binary_search_recursive(array, item, left=None, right=None):
