2d1ded0134d41e67a5fd934940fe7a879f935dde
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-06-12 13:43:21 -0700

    linear search complete

--- search.py
+++ search.py
@@ -18,9 +18,15 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
+
+    # base case
+    if array[index] == item:
+        return index
+    elif item not in array:
+        return None
+
+    # recursive case
+    return linear_search_recursive(array, item, index + 1)
 
 
 def binary_search(array, item):
