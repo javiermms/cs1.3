d86919a46aa29eb7de161f1870c2a8eb80a342ce
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-06-17 13:56:19 -0700

    completed all functions; work on edge case for find all indexes

--- strings.py
+++ strings.py
@@ -29,27 +29,29 @@ def contains(text, pattern):
             if pattern_count == pattern_length:
                 pattern_count = 0
 
-    print(count)
-    print(repeats)
+    # print(count)
+    # print(repeats)
 
     if repeats <= 0:
         return False
     else:
         return True
    
-    
-            
-           
-
-       
-
-
 def find_index(text, pattern):
     """Return the starting index of the first occurrence of pattern in text,
     or None if not found."""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
+
+    # for char in text:
+    #     if char == pattern[0]:
+    #         return text.index(char)
+
+    for num in range(0, len(text)):
+        if text[num] == pattern[0]:
+            return num
+
 
 
 def find_all_indexes(text, pattern):
@@ -58,7 +60,36 @@ def find_all_indexes(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    text_length = len(text)
+    pattern_length = len(pattern)
+    count= 0
+    pattern_count = 0
+    repeats = 0
+    index_array =[]
 
+    for num in range(0, text_length):
+
+        if text[num] == pattern[pattern_count]:
+            count += 1
+            index_array.append(num)
+            if count == pattern_length:
+                repeats += 1
+                count = 0
+        elif text[num] != pattern[pattern_count]:
+            count = 0
+            if text[num] == pattern[0]:
+                count = 1
+                pattern_count = 0
+        
+        if pattern_count <= pattern_length:
+            pattern_count += 1
+            if pattern_count == pattern_length:
+                pattern_count = 0
+    
+    if pattern_count == 0:
+        return index_array
+    
+    return index_array
 
 def test_string_algorithms(text, pattern):
     found = contains(text, pattern)
@@ -91,4 +122,6 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    print(contains('bananas', 'nas')) 
+    # print(contains('bananas', 'nas')) 
+    # print(find_index('bananas', 's'))
+    print(find_all_indexes('bananas', 'p'))
