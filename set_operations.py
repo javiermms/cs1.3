00991eaae3dee8d727e67c937aed790e6086ae8c
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-06-30 15:05:32 -0700

    finished union function

--- set_operations.py
+++ set_operations.py
@@ -14,40 +14,36 @@ class Set(object):
 		return 'Set: {!r} Size: {!r}'.format(self.elements, self.size)
 
 	def contains(self, element):
-		"""---"""
-		# TODO:return a boolean indicating whether element is in this set
-		# for item in self.elements:
-		# 	if item == element:
-		# 		return True
-		# return False
-		
+		"""Returns a boolean depending on if element is in the set"""
 		return element in self.elements
 
 
 	def add(self, element):
-		"""---"""
-		# TODO: add element to this set, if not present already
+		"""Inserts element into set"""
 		if not self.contains(element):
 			self.elements.append(element)
 			self.size += 1
 
 	def remove(self, element):
-		"""---"""
-		# TODO: remove element from this set, if present, or else raise KeyError
+		"""Deletes element from set"""
 		if not self.contains(element):
 			raise KeyError('Element {} not in set'.format(element))
 
 		index = self.elements.index(element)
 		self.elements.pop(index)
 		self.size -= 1
-		
-		
-		
 
 	def union(self, other_set):
-		"""---"""
-		# TODO: return a new set that is the union of this set and other_set
-		pass
+		"""Returns a new set of two sets added together"""
+		new_set = Set()
+
+		for element in other_set.elements:
+			new_set.add(element)
+		
+		for element in self.elements:
+			new_set.add(element)
+
+		return new_set
 
 	def intersection(self, other_set):
 		"""---"""
@@ -66,11 +62,18 @@ class Set(object):
 
 def test_set():
 	s = Set([1, 5, 9, 7, 3])
-	print(s)  #set size
-	s.add(10)
-	print(s) 
-	s.remove(9) 
-	print(s) 
+	r = Set([20, 30, 15, 7, 3])
+	# print(s)  #set size
+	# s.add(10)
+	# s.add('cheese')
+	# s.add(False)
+	# print(s) 
+	# s.remove(9)
+	# s.remove('cheese')
+	# s.remove(False) 
+	print(s)
+	print(r)
+	print(s.union(r))
 	
 if __name__ == '__main__':
 	test_set()
