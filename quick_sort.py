d0c01c8f91f528b4841195f8b7376313ad95d971
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-07-05 12:00:07 -0700

    init word jumble

--- quick_sort.py
+++ quick_sort.py
@@ -28,10 +28,11 @@ def partition_lumuto(array, lo, hi):
             i += 1
             # print(str(j) + ' '+str(i))
             # swap indexes if num greater than pivot
+            # #print(str(array[i]) +' '+ str(array[j])+' '+'less than pivot switch i & j')
             array[i], array[j] = array[j], array[i]
-            # print(str(array[i]) +' '+ str(array[j])+' '+'less than pivot switch i & j')
+            
     # swap pivot if partition is complete ()
-    # print(str(array[hi]) +' '+ str(array[i + 1])+' '+'switches pivot & i+1')
+    # #print(str(array[hi]) +' '+ str(array[i + 1])+' '+'switches pivot & i+1')
     array[i + 1], array[hi] = array[hi], array[i + 1]
     return i + 1
 
@@ -43,7 +44,7 @@ def quick_sort_hoare(array, lo, hi):
         p = partition_hoare(array, lo, hi)
         # Recursively sort the 'less than' partition
         quick_sort_hoare(array, lo, p)
-            # Recursively sort the 'greater than' partition
+        # Recursively sort the 'greater than' partition
         quick_sort_hoare(array, p+1, hi)
 
     return array
@@ -55,22 +56,27 @@ def partition_hoare(array, lo, hi):
     pivot = array[(hi + lo) // 2]
   
     while True:
+        # increments i towards pivot
         while True:
             i += 1
             if array[i] >= pivot:
                 break
-
+        # decrement j towards pivot
         while True:
             j -= 1
             if array[j] <= pivot:
                 break
 
         if i >= j:
+            print(str(j) + ' '+ 'var j')
             return j
 
+        print('Pivot: ' + str(pivot))
+        print(str(array[i]) +' '+ str(array[j]))
+        # swaps any index 
         array[i], array[j] = array[j], array[i]
 
-array = [3, 7, 8, 2, 1, 9, 5, 4, 9, 9, 7, 5, 20, 40, 80 , 100, 200, 34, 67, 97]
+array = [3, 7, 8, 25, 0, 68, 5, 32, 12, 29]
 # array = ['hello', 'apple', 'dog', 'cats']
 last_index = len(array) - 1
 # print(quick_sort_lumuto(array, 0, last_index))
