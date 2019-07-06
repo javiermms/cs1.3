fc5465e70c3350d2d3debbfcb87998e2db5aef94
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-07-05 21:57:32 -0700

    completed word_jumble

--- word_jumble.py
+++ word_jumble.py
@@ -1,3 +1,6 @@
+import time
+start = time.process_time()
+
 def get_file_lines(filename='/usr/share/dict/words'):
     """Return a list of strings on separate lines in the given text file with
     any leading and trailing whitespace characters removed from each line."""
@@ -6,8 +9,50 @@ def get_file_lines(filename='/usr/share/dict/words'):
         lines = [line.strip() for line in file]
     return lines
 
+def sort_dictionary(all_dictionary_words):
+    word_dic = dict()
+    
+    for word in all_dictionary_words:
+        sorted_words = ''.join(sorted(word.lower()))
+        word_dic[sorted_words] = word
+    
+    return word_dic
 
-def solve_word_jumble(words, circles, final):
+def sort_words(words):
+    jumble_sorted = []
+    for word in words:
+        sorted_words = ''.join(sorted(word.lower()))
+        jumble_sorted.append(sorted_words)
+    
+    return jumble_sorted
+
+def get_specific_indexes(array):
+    inner_arr = []
+    whole_arr = []
+    for element in range(len(array)):
+        for index, letter in enumerate(array[element]):
+            if letter == 'O':
+                inner_arr.append(index)
+            
+        whole_arr.append(inner_arr)
+        inner_arr = []
+    return whole_arr
+
+def get_word_count_and_size(array):
+    new_array = []
+    for element in array:
+        new_array.append(len(element))
+    return new_array
+
+def unjumble_words(jumble_sorted, word_dic):
+    answers = []
+    for word in jumble_sorted:
+        for key in word_dic.items():
+            if key[0] == word:
+                answers.append(key[1])
+    return answers
+
+def solve_word_jumble(words, circles, final, word_dic, jumble_sorted):
     """Solve a word jumble by unscrambling four jumbles, then a final jumble.
     Parameters:
     - words: list of strings, each is the scrambled letters for a single word
@@ -18,25 +63,54 @@ def solve_word_jumble(words, circles, final):
         2. _ (underscore) = the letter is not in the final jumble
     - final: list of strings in the same format as circles parameter that shows
         how the final jumble's letters are arranged into a word or phrase."""
-    # Get all English words in the built-in dictionary
-    all_words = get_file_lines()
-    # TODO: Solve this word jumble with data structures and algorithms
+    
+    # unjumbles word
+    answers = unjumble_words(jumble_sorted, word_dic)
+    # print(answers)
+
+    # gets needed index
+    whole_arr = get_specific_indexes(circles)
+    # print(whole_arr)
+
+    # gets the letter where the circles are
+    letter = ""
+    count = 0 #keep it on array until incremented to next
+    for element in whole_arr: #element is [2,4]
+        for _, num_in_array in enumerate(element): #num in array 2 then 4
+            letter += answers[count][num_in_array] 
+        count += 1
+
+    # print(letter)   
+
+    # get nums for letters
+    # word_count = get_word_count_and_size(get_specific_indexes(final))
+    # print(word_count)
+    
+    return answers
 
 
 def main():
+    sorted_dic = sort_dictionary(get_file_lines())
+    
     # Word Jumble 1. Cartoon prompt for final jumble:
     # "Farley rolled on the barn floor because of his ___."
     words1 = ['TEFON', 'SOKIK', 'NIUMEM', 'SICONU']
     circles1 = ['__O_O', 'OO_O_', '____O_', '___OO_']
     final1 = ['OO', 'OOOOOO']
-    solve_word_jumble(words1, circles1, final1)
+    
+    sorted_jumble = sort_words(words1)
 
-    # Word Jumble 2. Cartoon prompt for final jumble: "What a dog house is."
+    print(solve_word_jumble(words1, circles1, final1, sorted_dic, sorted_jumble))
+
+    # # Word Jumble 2. Cartoon prompt for final jumble: "What a dog house is."
     words2 = ['TARFD', 'JOBUM', 'TENJUK', 'LETHEM']
     circles2 = ['____O', '_OO__', '_O___O', 'O____O']
     final2 = ['OOOO', 'OOO']
-    solve_word_jumble(words2, circles2, final2)
 
+    sorted_jumble = sort_words(words2)
+
+    print(solve_word_jumble(words2, circles2, final2, sorted_dic, sorted_jumble))
 
 if __name__ == '__main__':
     main()
+    print('Time:'+' '+str(time.process_time() - start))
